c0dc40722f53ce04befc1e45238d1577b9ba2df8
Daniel <91342168+Danielperfull-00@users.noreply.github.com>
2023-05-03 22:40:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ const color = (text, color) => {
   return !color ? chalk.green(text) : chalk.keyword(color)(text);
 };
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello, world!');
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
 function smsg(conn, m, store) {
   if (!m) return m;
   let M = proto.WebMessageInfo;
